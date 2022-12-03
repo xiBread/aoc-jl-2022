63c0a15a2df3a39134f82e6b97329681e43256aa
Oscar Lee-Vermeren <oscar.leevermeren@gmail.com>
2022-12-03 09:44:51 -0500

    day 3

---
+++ src/03.jl
@@ -0,0 +1,11 @@
+priority(chr) = islowercase(chr) ? chr - 'a' + 1 : chr - 'A' + 27
+
+input = readlines("inputs/03.txt")
+
+output() = (
+    sum(priority(first(line[1:end÷2] ∩ line[end÷2+1:end])) for line ∈ input),
+    sum(priority(first(∩(input[i:i+2]...))) for i ∈ 1:3:length(input))
+)
+
+@time output()
+@show output()
