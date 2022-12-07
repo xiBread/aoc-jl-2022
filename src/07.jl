b9816ee2d4f77198680fb86349dfee546676d04b
Oscar Lee-Vermeren <oscar.leevermeren@gmail.com>
2022-12-07 01:11:09 -0500

    day 7

---
+++ src/07.jl
@@ -0,0 +1,26 @@
+function day07()
+    paths = [""]
+    sizes = Dict{String,Int64}()
+    freespace = -40_000_000
+
+    for line ∈ eachline("inputs/07.txt")
+        if startswith(line, "\$ cd ..")
+            pop!(paths)
+        elseif startswith(line, "\$ cd ")
+            push!(paths, "$(last(paths))/$line")
+        elseif isdigit(line[1])
+            size = parse(Int, split(line, " ")[1])
+            freespace += size
+
+            mergewith!(+, sizes, Dict(path => size for path ∈ paths))
+        end
+    end
+
+    vals = collect(values(sizes))
+
+    sum(filter(i -> i ≤ 100_000, vals)),
+    minimum(filter(i -> i ≥ freespace, vals))
+end
+
+@time day07()
+@show day07()
